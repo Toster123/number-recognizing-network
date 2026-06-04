4e24de3ebb552f5282c841cabdc7cf044222ad9b
Toster123 <mr.ilya.grigoriev@gmail.com>
2026-06-04 22:15:23 +0300

    feat: improve screen

--- src/screen/screen.py
+++ src/screen/screen.py
@@ -14,29 +14,36 @@ class Screen(tk.Tk):
         self.title('Number recognition')
         self.network = network
 
-        self.x = self.y = 0
-
         self.line_id = None
         self.line_points = []
         self.line_options = {}
 
+        self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}+0+0")
+
+        self.grid_propagate(False)
+
         self.canvas = tk.Canvas(self, width=500, height=500, bg="white", cursor="cross")
-        self.result_0 = tk.Label(self, text="...")
-        self.result_1 = tk.Label(self, text="...")
-        self.result_2 = tk.Label(self, text="...")
-        self.result_3 = tk.Label(self, text="...")
-        self.result_4 = tk.Label(self, text="...")
-        self.result_5 = tk.Label(self, text="...")
-        self.result_6 = tk.Label(self, text="...")
-        self.result_7 = tk.Label(self, text="...")
-        self.result_8 = tk.Label(self, text="...")
-        self.result_9 = tk.Label(self, text="...")
+        self.result_0 = tk.Label(self, text="...", width=40)
+        self.result_1 = tk.Label(self, text="...", width=40)
+        self.result_2 = tk.Label(self, text="...", width=40)
+        self.result_3 = tk.Label(self, text="...", width=40)
+        self.result_4 = tk.Label(self, text="...", width=40)
+        self.result_5 = tk.Label(self, text="...", width=40)
+        self.result_6 = tk.Label(self, text="...", width=40)
+        self.result_7 = tk.Label(self, text="...", width=40)
+        self.result_8 = tk.Label(self, text="...", width=40)
+        self.result_9 = tk.Label(self, text="...", width=40)
         self.classify_button = tk.Button(self, text="Recognize", command=self.predict_number)
         self.clear_button = tk.Button(self, text="Clear", command=self.clear)
         self.fit_network_status = tk.Label(self, text="")
 
+        self.ep_label = tk.Label(self, text="Epochs")
+        self.batch_label = tk.Label(self, text="Batch")
+        self.dataset_label = tk.Label(self, text="Dataset %")
+        self.lr_label = tk.Label(self, text="LR")
+
         self.epochs_input = tk.Spinbox(self, from_=1, to=100, textvariable=tk.IntVar(value=10))
-        self.batch_size_input = tk.Spinbox(self, from_=1, to=5000, increment=16, textvariable=tk.IntVar(value=8))
+        self.batch_size_input = tk.Spinbox(self, from_=1, to=5000, increment=8, textvariable=tk.IntVar(value=16))
         self.dataset_size_input = tk.Spinbox(self, from_=0, to=1, increment=0.01, textvariable=tk.DoubleVar(value=0.03))
         self.learning_rate_input = tk.Spinbox(self, from_=0.001, to=0.5, increment=0.001, textvariable=tk.DoubleVar(value=0.01))
         self.fit_network_button = tk.Button(self, text="Start fitting", command=self.start_fitting)
@@ -48,33 +55,45 @@ class Screen(tk.Tk):
         self.progress_loop = None
 
         # Сетка окна
-        self.canvas.grid(row=0, rowspan=10, column=0, columnspan=2, pady=2, sticky=W)
-        self.result_0.grid(row=0, column=2, pady=2, padx=2)
-        self.result_1.grid(row=1, column=2, pady=2, padx=2)
-        self.result_2.grid(row=2, column=2, pady=2, padx=2)
-        self.result_3.grid(row=3, column=2, pady=2, padx=2)
-        self.result_4.grid(row=4, column=2, pady=2, padx=2)
-        self.result_5.grid(row=5, column=2, pady=2, padx=2)
-        self.result_6.grid(row=6, column=2, pady=2, padx=2)
-        self.result_7.grid(row=7, column=2, pady=2, padx=2)
-        self.result_8.grid(row=8, column=2, pady=2, padx=2)
-        self.result_9.grid(row=9, column=2, pady=2, padx=2)
-        self.clear_button.grid(row=10, column=0, pady=2)
-        self.classify_button.grid(row=10, column=1, columnspan=2, pady=2, padx=2)
-        self.fit_network_status.grid(row=11, column=1, pady=2, padx=2)
+        for i in range(10):
+            self.rowconfigure(i, minsize=60, uniform="row_group")
+        
+        self.columnconfigure(2, weight=30, uniform="col_group")
+        for i in range(3, 9):
+            self.columnconfigure(i, weight=40, uniform="col_group")
 
-        self.epochs_input.grid(row=0, column=3, pady=2, padx=2)
-        self.batch_size_input.grid(row=1, column=3, pady=2, padx=2)
-        self.dataset_size_input.grid(row=2, column=3, pady=2, padx=2)
-        self.learning_rate_input.grid(row=3, column=3, pady=2, padx=2)
-        self.fit_network_button.grid(row=4, column=3, pady=2, padx=2)
-        self.stop_fitting_button.grid(row=5, column=3, pady=2, padx=2)
-        self.fit_network_log.grid(row=6, rowspan=6, column=3, columnspan=2, pady=2, sticky=W)
+        self.canvas.grid(row=0, rowspan=10, column=0, columnspan=2, pady=2, sticky=W)
+        self.result_0.grid(row=0, column=2, pady=2, sticky=NSEW)
+        self.result_1.grid(row=1, column=2, pady=2, sticky=NSEW)
+        self.result_2.grid(row=2, column=2, pady=2, sticky=NSEW)
+        self.result_3.grid(row=3, column=2, pady=2, sticky=NSEW)
+        self.result_4.grid(row=4, column=2, pady=2, sticky=NSEW)
+        self.result_5.grid(row=5, column=2, pady=2, sticky=NSEW)
+        self.result_6.grid(row=6, column=2, pady=2, sticky=NSEW)
+        self.result_7.grid(row=7, column=2, pady=2, sticky=NSEW)
+        self.result_8.grid(row=8, column=2, pady=2, sticky=NSEW)
+        self.result_9.grid(row=9, column=2, pady=2, sticky=NSEW)
+        self.clear_button.grid(row=10, column=0, pady=2)
+        self.classify_button.grid(row=10, column=1, pady=2, padx=2)
+
+        self.ep_label.grid(row=0, column=3, pady=2, padx=2)
+        self.batch_label.grid(row=0, column=4, pady=2, padx=2)
+        self.dataset_label.grid(row=0, column=5, pady=2, padx=2)
+        self.lr_label.grid(row=0, column=6, pady=2, padx=2)
+        
+        self.epochs_input.grid(row=1, column=3, pady=2, padx=2)
+        self.batch_size_input.grid(row=1, column=4, pady=2, padx=2)
+        self.dataset_size_input.grid(row=1, column=5, pady=2, padx=2)
+        self.learning_rate_input.grid(row=1, column=6, pady=2, padx=2)
+        self.fit_network_button.grid(row=1, column=7, pady=2, padx=2)
+        self.stop_fitting_button.grid(row=1, column=8, pady=2, padx=2)
+        self.fit_network_log.grid(row=2, rowspan=10, column=3, columnspan=6, pady=2, ipadx=110, ipady=110, sticky=W)
+        self.fit_network_status.grid(row=12, column=3, columnspan=6, pady=2, padx=2)
 
         self.canvas.bind('<Button-1>', self.start_line)
         self.canvas.bind('<B1-Motion>', self.draw_line)
         self.canvas.bind('<ButtonRelease-1>', self.end_line)
-
+        
         mainloop()
 
     def clear(self):
@@ -86,12 +105,12 @@ class Screen(tk.Tk):
         self.fit_network_status.configure(text='')
     
     def stop_fitting(self):
-        if self.fit_loop is not None and self.fit_task is not None and not self.fit_task.done():
-            self.fit_task.cancel()
-            self.fit_loop.stop()
         if self.progress_loop is not None and self.show_task is not None and not self.show_task.done():
             self.show_task.cancel()
             self.progress_loop.stop()
+        if self.fit_loop is not None and self.fit_task is not None and not self.fit_task.done():
+            self.fit_task.cancel()
+            self.fit_loop.stop()
 
         self.fit_task = None
         self.show_task = None
@@ -138,6 +157,8 @@ class Screen(tk.Tk):
 
         # Отдаём строки в UI по мере готовности
         async for _, bar_str in bridge.stream():
+            await asyncio.sleep(0)
+
             if bar_str is None:
                 progress += str(latest_bar_str) + '\n'
             else:
